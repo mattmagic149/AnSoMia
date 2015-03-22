9f72f62fb745a2b5ece0672ee8379c6e47fa6be6
Matthias Ivantsits <matthias.ivantsits@student.tugraz.at>
2015-03-22 16:44:08 +0100

    started dailycrawler

--- AnSoMnia/src/General/IndexCrawler.java
+++ AnSoMnia/src/General/IndexCrawler.java
@@ -18,16 +18,20 @@ public class IndexCrawler
     Document document = builder.parse( new File("test/google.html") );
     System.out.println( document.getFirstChild().getTextContent() );*/
 	  
-	  String url = "https://de.finance.yahoo.com/q?s=";
-	  String company_ticker = "";
+	  String url = "https://de.finance.yahoo.com/";
+	  String query_page_ks = "ks?s=";
+	  String query_page_q = "q?s=";
+	  String company_ticker = "RKET";
+	  String query_result = "&ql=1";
 	  
-	  org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
-	  Elements navigation = doc.select("#yfi_quote_summary_data tr");
+	  org.jsoup.nodes.Document doc = Jsoup.connect(url + query_page_q + company_ticker + query_result).get();
+	  Elements stock_price_query = doc.select(".yfi_rt_quote_summary_rt_top span");
 	  
-	  
+	  float stock_price = Float.parseFloat(stock_price_query.get(0).child(0).html());
 	  
 	  /*for(int i = 0; i < key_data.size(); i++) {
 		  System.out.println(key_data.get(i));
 	  }*/
   }
+  
 }
