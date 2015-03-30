1b48ae04eecc6e00e9f94b479e067203a0324e6e
Simon Senkl <development@simonsenkl.at>
2015-03-30 22:07:10 +0200

    Updated kpi.txt and KeyPerformanceIndicators (edited variables / types and added constructor)

--- AnSoMnia/src/General/KeyPerformanceIndicators.java
+++ AnSoMnia/src/General/KeyPerformanceIndicators.java
@@ -20,50 +20,50 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
 	private long id;
-	
 	private Date date;
-	private float earnings_per_share;
-	private float earnings_per_share_growth; ///TODO: class EarningsPerShareGrowth existed, but it was not included in the kpi.txt file???
-	private long earnings_ratio;
-	private float price_earnings_ratio; ///TODO: class PriceEarningsRatio existed, but it was not included in the kpi.txt file???
-	private long earnings_to_growth_ratio;
-	private float price_earnings_to_growth_ratio; ///TODO: class PriceEarningsToGrowthRatio existed, but it was not included in the kpi.txt file???
-	private long price_ratio;
-	private float cashflow_per_share;
-	private float price_cash_flow_ratio;
-	private float price_to_book_value;
-	private float price_sales_ratio;
-	//private long equity_ratio;
-	private float debt_ratio;
-	private float return_on_equity;
-	private float payout_ratio;
-	private float gross_margin;
-	private float operating_margin;
-	private float book_value_per_share;
-	private float dividend_price_ratio; ///TODO: class DividendPriceRatio existed, but it was not included in the kpi.txt file???
 	
-	private long alpha;
-	private long beta;
-	private float sharpe;
-	
-	private long revenue;
-	private long operating_income;
-	private long net_income;
-	private long dividend;
-	private long outstanding_shares;
-	private long cashflow;
-	private long working_capital;
-	private long book_value;
-	private float equity_ratio;
-	private long equity;
-	private long debt;
-	private long balance_sheet_total;
-	private long market_capitalisation;
-	private long gross_profit;
-	private long liquidity;
+	// KPIs to crawl
+	private float balance_sheet_total;
+	private float cashflow;
+	private float debt;
+	private float dividend;
+	private float equity;
+	private float gross_profit;
+	private float liquidity;
+	private float net_income;
 	private long number_of_employees;
+	private float operating_income;
+	private long outstanding_shares;
+	private float revenue;
+	private float working_capital;
 	
+	// KPIs to calculate
+	//private float alpha;
+	//private float beta;
+	private float book_value_per_share;
+	private float cashflow_per_share;
+	private float debt_ratio;
+	private float dividend_price_ratio; ///TODO: class DividendPriceRatio existed, but it was not included in the kpi.txt file???
+	private float earnings_per_share;
+	private float earnings_per_share_growth; // Is NOW included in kpi.txt (at the top, with all the other KPIs to calculate)
+	private float equity_ratio;
+	private float gross_margin;
+	private float market_capitalisation;
+	private float operating_margin;
+	private float payout_ratio;
+	private float price_cash_flow_ratio;
+	private float price_earnings_ratio; // Already is included in kpi.txt (at the top, with all the other KPIs to calculate)
+	private float price_earnings_to_growth_ratio; // Already is included in kpi.txt (at the top, with all the other KPIs to calculate)
+	private float price_sales_ratio;
+	private float price_to_book_value;
+	private float return_on_equity;
+	//private float sharpe;
 	
+	// TODO: categorize or delete
+	private long earnings_ratio; // what da fuck is dat?
+	private long earnings_to_growth_ratio; // what da fuck is dat?
+	private long price_ratio; // what da fuck is dat?
+
 	@ManyToOne
 	@JoinColumn(name="isin",updatable=false)
 	protected SingleCompany company;
@@ -71,6 +71,69 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 	public KeyPerformanceIndicators() {
 		this.date = new Date();
 	}
+	
+	public KeyPerformanceIndicators(long total, long equity, long cashflow, long debt, long dividend, long gross_profit,long liquidity,
+			long net_income, int number_of_employees, long operating_income, int shares, long revenue, long working_capital, long price, long past_net_income) {
+		
+		// initialize date
+		this.date = new Date();
+		
+		// initialize crawled KPIs
+		this.balance_sheet_total = total;
+		this.cashflow = cashflow;
+		this.debt = debt;
+		this.dividend = dividend;
+		this.equity = equity;
+		this.gross_profit = gross_profit;
+		this.liquidity = liquidity;
+		this.net_income = net_income;
+		this.number_of_employees = number_of_employees;	
+		this.operating_income = operating_income;
+		this.outstanding_shares = shares;
+		this.revenue = revenue;
+		this.working_capital = working_capital;
+		
+		// initialize directly calculated KPIs
+		this.book_value_per_share = this.calculateBookValuePerShare(equity, shares);
+		this.cashflow_per_share = this.calculateCashflowPerShare(cashflow, shares);
+		this.debt_ratio = this.calculateDebtRatio(debt, total);
+		this.dividend_price_ratio = this.calculateDividendPriceRatio(dividend, price);
+		this.earnings_per_share = this.calculateEarningsPerShare(net_income, shares);
+		this.earnings_per_share_growth = this.calculateEarningsPerShareGrowth(net_income, past_net_income);
+		this.equity_ratio = this.calculateEquityRatio(equity, total);
+		this.gross_margin = this.calculateGrossMargin(gross_profit, revenue);
+		this.market_capitalisation = this.calculateMarketCapitalization(price, shares); 
+		this.operating_margin = this.calculateOperatingMargin(operating_income, revenue);
+		this.price_cash_flow_ratio = this.calculatePriceCashflowRatio(price, cashflow);
+		this.price_earnings_ratio = this.calculatePriceEarningsRatio(price, net_income);
+		this.price_sales_ratio = this.calculatePriceSalesRatio(price, revenue, shares);
+		this.price_to_book_value = this.calculatePriceToBookValue(price, equity, shares);
+		this.return_on_equity = this.calculateReturnOnEquityRatio(net_income, equity);
+		
+		// initialize derivative KPIs
+		this.payout_ratio = this.calculatePayoutRatio(dividend, earnings_per_share);
+		this.price_earnings_to_growth_ratio = this.calculatePriceEarningsToGrowthRatio(price_earnings_ratio, earnings_per_share_growth);
+		/*
+		 * this.alpha = this.calculateAlpha(); 
+		 * this.beta = this.calculateBeta(); 
+		 * this.sharpe = this.calculateSharpe();
+		 */
+		
+	}
+	
+	/*
+	// TODO: Implement Alpha
+	private float calculateAlpha() {
+		return -1.0f;
+	}
+	*/
+	
+	/*
+	// TODO: Implement Beta
+	private float calculateBeta() {
+		return -1.0f;
+	}
+	*/
 	
 	private float calculateBookValuePerShare(float book_value, int shares) {
 		return (book_value / shares);
@@ -80,7 +143,7 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 		return (cashflow / shares);
 	}
 	
-	private float calculateDebtRatio(float debt, int total) {
+	private float calculateDebtRatio(float debt, float total) {
 		return (debt / total);
 	}
 	
@@ -92,7 +155,7 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 		return (earnings / shares);
 	}
 	
-	private float calculateEarningsPerShareGrowth(float current_earnings, int past_earnings) {
+	private float calculateEarningsPerShareGrowth(float current_earnings, float past_earnings) {
 		return (current_earnings / past_earnings);
 	}
 	
@@ -102,6 +165,10 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 	
 	private float calculateGrossMargin(float gross, float sales) {
 		return (gross / sales);
+	}
+	
+	private float calculateMarketCapitalization(float price, int shares) {
+		return (price * shares);
 	}
 	
 	private float calculateOperatingMargin(float operating_income, float sales) {
@@ -136,6 +203,8 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 		return (net / equity);
 	}
 	
+	/*
+	// TODO: Implement Sharpe without ArrayLists monthly_return and riskfree as parameters
 	private float calculateSharpe(ArrayList<Float> monthly_return, ArrayList<Float> riskfree) {
 		int last = monthly_return.size() - 1;
 		float average_return;
@@ -160,8 +229,10 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 		
 		return (average_return / volatility);
 	}
+	*/
 	
-	/*public static void main( String[] args ) {
+	/*
+	public static void main( String[] args ) {
 		ArrayList<Float> returns = new ArrayList<Float>();
 		returns.add(1.1f);
 		returns.add(1.2f);
@@ -177,7 +248,8 @@ public class KeyPerformanceIndicators implements ISaveAndDelete {
 		
 		Sharpe test = new Sharpe(company, returns, riskfree, date);
 		System.out.println(test.sharpe);
-	}*/
+	}
+	*/
 	
 	@Override
 	public boolean saveToDB() {
